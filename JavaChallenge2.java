b50dca32f3580c30d67e61069ed2bac57f6820bd
Menaka-06 <126381870+Menaka-06@users.noreply.github.com>
2023-03-17 11:52:17 +0530

    Add files via upload
    challenge2

---
+++ JavaChallenge2.java
@@ -0,0 +1,10 @@
+package javachallenge;
+public class JavaChallenge2 {
+	public static void main(String[] args) {
+		//squareroot of x
+		int x=10;
+		  double sqrt = Math.sqrt(x);
+		  int value = (int)sqrt;
+		System.out.println("the square root of "+x+" x is= "+value);
+		}
+	}
